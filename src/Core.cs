b9fa9472dd65641ed4eaf34f57ff41148cb7fbf1
DArkHekRoMaNT <darkhekromant@yandex.ru>
2022-09-27 11:22:44 +0300

    Move TavisExtensions to separate file

--- src/Core.cs
+++ src/Core.cs
@@ -105,38 +105,3 @@ namespace TeleportationNetwork
         }
     }
 }
-//Credit to Apache on Vintage Story Discord
-/// <summary>
-///     Extension methods for Tavis JSON Patching Engine.
-/// </summary>
-public static class TavisExtensions
-{
-    private static int _dummyValue = 0;
-
-    /// <summary>
-    ///     Applies a single patch to a JSON file.
-    /// </summary>
-    /// <param name="api">The core API used by the game, on both the client, and the server.</param>
-    /// <param name="patch">The patch to apply.</param>
-    public static void ApplyJsonPatch(this ICoreAPI api, JsonPatch patch)
-    {
-        // Still using these awkward pass by reference dummy values.
-        // Ideally, the part of the method that actually adds the patch should be extracted.
-        var jsonPatcher = api.ModLoader.GetModSystem<ModJsonPatchLoader>();
-        jsonPatcher.ApplyPatch(0, patch.File, patch, ref _dummyValue, ref _dummyValue, ref _dummyValue);
-    }
-
-    /// <summary>
-    ///     Applies a number of patches to the JSON assets of the game.
-    /// </summary>
-    /// <param name="api">The core API used by the game, on both the client, and the server.</param>
-    /// <param name="patches">The patches to apply.</param>
-    public static void ApplyJsonPatches(this ICoreAPI api, IEnumerable<JsonPatch> patches)
-    {
-        var jsonPatcher = api.ModLoader.GetModSystem<ModJsonPatchLoader>();
-        foreach (var patch in patches)
-        {
-            jsonPatcher.ApplyPatch(0, patch.File, patch, ref _dummyValue, ref _dummyValue, ref _dummyValue);
-        }
-    }
-}
