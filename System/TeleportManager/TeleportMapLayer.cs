e273dc05701a94b8612c685caa67f30a5284fbda
DArkHekRoMaNT <darkhekromant@yandex.ru>
2023-04-05 17:03:05 +0300

    Use waypoints data from WaypointMapLayer

--- System/TeleportManager/TeleportMapLayer.cs
+++ System/TeleportManager/TeleportMapLayer.cs
@@ -1,11 +1,7 @@
-using Cairo;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Vintagestory.API.Client;
-using Vintagestory.API.Config;
-using Vintagestory.API.MathTools;
-using Vintagestory.API.Util;
 using Vintagestory.GameContent;
 
 namespace TeleportationNetwork
@@ -13,35 +9,12 @@ namespace TeleportationNetwork
     public class TeleportMapLayer : MapLayer
     {
         private readonly List<MapComponent> _components = new();
+        private readonly WaypointMapLayer _waypointMapLayer;
 
-        public Dictionary<string, LoadedTexture> TexturesByIcon { get; } = new();
-        public string[] WaypointIcons { get; } = new string[]
-        {
-            "circle",
-            "bee",
-            "cave",
-            "home",
-            "ladder",
-            "pick",
-            "rocks",
-            "ruins",
-            "spiral",
-            "star1",
-            "star2",
-            "trader",
-            "vessel"
-        };
-
-        public List<int> WaypointColors { get; }
-        private static readonly string[] _hexColors = new string[]
-        {
-            "#F9D0DC", "#F179AF", "#F15A4A", "#ED272A", "#A30A35", "#  FFDE98", "#EFFD5F", "#F6EA5E", "#FDBB3A", "#C8772E",
-            "#F47832", "C3D941", "#9FAB3A", "#94C948", "#47B749", "#366E4F", "#516D66", "93D7E3", "#7698CF", "#20909E",
-            "#14A4DD", "#204EA2", "#28417A", "#C395C4", "#92479B", "#8E007E", "#5E3896", "D9D4CE", "#AFAAA8", "#706D64",
-            "#4F4C2B", "#BF9C86", "#9885530", "#5D3D21", "#FFFFFF", "#080504"
-        };
-
-        public MeshRef QuadModel { get; private set; }
+        public Dictionary<string, LoadedTexture> TexturesByIcon => _waypointMapLayer.texturesByIcon;
+        public List<int> WaypointColors => _waypointMapLayer.WaypointColors;
+        public MeshRef QuadModel => _waypointMapLayer.quadModel;
+        public string[] WaypointIcons => _waypointMapLayer.WaypointIcons.Select(e => e.Key).ToArray();
 
         public override string Title => "TpNetOverlay";
         public override EnumMapAppSide DataSide => EnumMapAppSide.Client;
@@ -51,13 +24,8 @@ namespace TeleportationNetwork
             var manager = api.ModLoader.GetModSystem<TeleportManager>();
             manager.Points.Changed += RebuildMapComponents;
 
-            QuadModel = api.Render.UploadMesh(QuadMeshUtil.GetQuad());
-
-            WaypointColors = new List<int>();
-            for (int i = 0; i < _hexColors.Length; i++)
-            {
-                WaypointColors.Add(ColorUtil.Hex2Int(_hexColors[i]));
-            }
+            var mapLayers = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers;
+            _waypointMapLayer = (WaypointMapLayer)mapLayers.FirstOrDefault(ml => ml is WaypointMapLayer);
         }
 
         public override void Render(GuiElementMap mapElem, float dt)
@@ -73,42 +41,6 @@ namespace TeleportationNetwork
 
         public override void OnMapOpenedClient()
         {
-            if (TexturesByIcon.Count == 0)
-            {
-                if (TexturesByIcon.Count > 0)
-                {
-                    foreach (var val in TexturesByIcon)
-                    {
-                        val.Value.Dispose();
-                    }
-                }
-
-                double scale = RuntimeEnv.GUIScale;
-                int size = (int)(27 * scale);
-
-                var surface = new ImageSurface(Format.Argb32, size, size);
-                var ctx = new Context(surface);
-
-                ICoreClientAPI capi = (ICoreClientAPI)api;
-
-                foreach (var val in WaypointIcons)
-                {
-                    ctx.Operator = Operator.Clear;
-                    ctx.SetSourceRGBA(0, 0, 0, 0);
-                    ctx.Paint();
-                    ctx.Operator = Operator.Over;
-
-                    capi.Gui.Icons.DrawIcon(ctx, "wp" + val.UcFirst(), 1, 1, size - 2, size - 2, new double[] { 0, 0, 0, 1 });
-                    capi.Gui.Icons.DrawIcon(ctx, "wp" + val.UcFirst(), 2, 2, size - 4, size - 4, ColorUtil.WhiteArgbDouble);
-
-                    TexturesByIcon[val] = new LoadedTexture(capi, capi.Gui.LoadCairoTexture(surface, false), (int)(20 * scale), (int)(20 * scale));
-                }
-
-                ctx.Dispose();
-                surface.Dispose();
-            }
-
-
             RebuildMapComponents();
         }
 
